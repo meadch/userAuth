dc73883693ae989064b21d4f24339a73331010b2
meadch <meadch@gmail.com>
2016-09-27 10:09:23 -0700

    adds registration validation logic

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const router = require('express').Router(),
+      // To deal w/ file uploads
+      multer = require('multer'),
+      upload = multer({dest: './uploads'});
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -9,9 +12,32 @@ router.get('/', function(req, res, next) {
 router.get('/register', function(req, res, next) {
   res.render('register', { title: "Register" });
 });
+router.post('/register', upload.single('profile_image'), function(req, res, next) {
+  const { name, email, username, password, password_confirm } = req.body;
+  const profileImage = ( req.file ) ? req.file.filename : 'noimage.jpg';
+
+  // Form validation
+  const errors = validateRegistration(req);
+
+  if (errors) {
+    res.render('register', { errors })
+  } else {
+    console.log('no errors')
+  }
+});
 
 router.get('/login', function(req, res, next) {
   res.render('login', { title: "Login" });
 });
+
+function validateRegistration(req){
+  req.checkBody('name', 'Name field is required').notEmpty();
+  req.checkBody('email', 'Email field is required').notEmpty();
+  req.checkBody('email', 'Email field is invalid').isEmail();
+  req.checkBody('username', 'Username field is required').notEmpty();
+  req.checkBody('password', 'Password field is required').notEmpty();
+  req.checkBody('password_confirm', 'Passwords must match').equals(req.body.password);
+  return req.validationErrors();
+}
 
 module.exports = router;
