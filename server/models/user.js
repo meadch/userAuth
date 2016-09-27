dfbdb3b5f774261ebb0d3b97b5a76b2169a65a4d
meadch <meadch@gmail.com>
2016-09-27 12:11:42 -0700

    begins bcrypt

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose'),
-      DB_URL = 'mongodb://localhost/nodeauth';
+    DB_URL = 'mongodb://localhost/nodeauth',
+    bcrypt = require('bcryptjs');
 
 // mpromise (mongoose's default promise library) is deprecated, plug in your own promise library
 mongoose.Promise = global.Promise;
@@ -8,56 +9,69 @@ mongoose.connect(DB_URL);
 const db = mongoose.connection;
 
 const UserSchema = mongoose.Schema({
-  username: {
-    type: String,
-    index: true
-  },
-  password: {
-    type: String
-  },
-  email: {
-    type: String
-  },
-  name: {
-    type: String
-  },
-  profileImage: {
-    type: String
-  }
+    username: {
+        type: String,
+        index: true
+    },
+    password: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    name: {
+        type: String
+    },
+    profileImage: {
+        type: String
+    }
 });
 
 const User = mongoose.model('User', UserSchema);
 
-module.exports = (function(){
-  const UserController = {};
+module.exports = (function() {
+    const UserController = {};
 
-  UserController.create = (req, res) => {
-    res.json(['hello'])
-  }
-
-  UserController.validateRegistration = (req, res) => {
-    req.checkBody('name', 'Name field is required').notEmpty();
-    req.checkBody('email', 'Email field is required').notEmpty();
-    req.checkBody('email', 'Email field is invalid').isEmail();
-    req.checkBody('username', 'Username field is required').notEmpty();
-    req.checkBody('password', 'Password field is required').notEmpty();
-    req.checkBody('password_confirm', 'Passwords must match').equals(req.body.password);
-
-    if (!req.validationErrors()) {
-        req.check('email', 'Email Taken').isEmailAvailable();
+    UserController.index = (req, res) => {
+        User.find({})
+            .then((users) => {
+                res.json(users);
+            })
     }
 
-    req.asyncValidationErrors()
-        .then( () => {
-            UserController.create(req, res);
-        })
-        .catch( (errors) => {
-          errors.forEach( ({msg}) => {
-            req.flash('error flash', msg);
-          })
-          res.redirect('/users/register');
+    UserController.create = (req, res) => {
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            bcrypt.hash(myPlaintextPassword, salt, function(err, hash) {
+                // Store hash in your password DB.
+            });
         });
-  }
+    }
 
-  return UserController;
+    UserController.validateRegistration = (req, res) => {
+        req.checkBody('name', 'Name field is required').notEmpty();
+        req.checkBody('email', 'Email field is required').notEmpty();
+        req.checkBody('email', 'Email field is invalid').isEmail();
+        req.checkBody('username', 'Username field is required').notEmpty();
+        req.checkBody('password', 'Password field is required').notEmpty();
+        req.checkBody('password_confirm', 'Passwords must match').equals(req.body.password);
+
+        if (!req.validationErrors()) {
+            req.check('email', 'Email Taken').isEmailAvailable();
+        }
+
+        req.asyncValidationErrors()
+            .then(() => {
+                UserController.create(req, res);
+            })
+            .catch((errors) => {
+                errors.forEach(({
+                    msg
+                }) => {
+                    req.flash('error flash', msg);
+                })
+                res.redirect('/users/register');
+            });
+    }
+
+    return UserController;
 }());
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,22 +1,17 @@
 const router = require('express').Router(),
-    // To deal w/ file uploads
-    multer = require('multer'),
-    upload = multer({
-        dest: './uploads'
-    }),
-    UserController = require('../models/user')
-
+      // To deal w/ file uploads
+      multer = require('multer'),
+      upload = multer({
+          dest: './uploads'
+      }),
+      UserController = require('../models/user');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-    User.find()
-        .then((users) => res.json(users))
-});
+router.get('/', UserController.index );
 
-router.get('/register', function(req, res, next) {
+router.get('/register', function(req, res) {
   res.render('register', { title: "Register" });
 });
-
 router.post('/register', upload.single('profile_image'), UserController.validateRegistration )
 
 router.get('/login', function(req, res, next) {
