6181e367dc92f0e06d5f8ce360504f01a19293e6
meadch <meadch@gmail.com>
2016-10-15 18:36:28 +0000

    fixes configuration for ubuntu

--- server/index.js
+++ server/index.js
@@ -62,8 +62,8 @@ app.get('*', (req, res, next) => {
   next();
 });
 
-app.use('/auth', mainRouter);
-app.use('/auth/users', usersRouter);
+app.use('/users', usersRouter);
+app.use('/', mainRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
